e4fb2e0065af4606f97936e551445cdc2ca85c4b
Nathan McBride <nmcbride@protonmail.com>
2019-09-28 16:11:34 -0400

    All requirements implemented and program is functioning normally

--- rockpaperscissors.c
+++ rockpaperscissors.c
@@ -1,26 +1,45 @@
+//
+// ======================
+// Nathan Mcbride 0615415
+// Rock Paper Scissors!
+// COIS-3320 Lab 1
+// ======================
+//
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>``
+#include <ctype.h>
+#include <time.h>
+
+#define infinate 1
 
 char GetUserInput()
 {
-  char input;
-  printf("\nEnter your Guess -> ");
-  scanf("%c", &input);
-  // Input checking?
-  return input;
+  char Prompt()
+  {
+    char input;
+    printf("Enter your Guess -> ");
+    scanf("%c", &input);
+    getchar(); // Grab the extra character entered
+    return toupper(input);
+  }
+
+  while(infinate)
+  {
+    char input = Prompt();
+    if(input == 'R' || input == 'P' || input == 'S' || input == 'Q') { return input; }
+    else { printf("Invalid Input\n"); }
+  }
 }
 
 char GetComputerChoice()
 {
-  char computer_choice;
-  int r = rand() % 3;
-  switch(r)
+  switch((rand() % 3))
   {
-    case 0 : computer_choice = 'P'; break;
-    case 1 : computer_choice = 'R'; break;
-    case 2 : computer_choice = 'S'; break;
+    case 0 : return 'P'; break;
+    case 1 : return 'R'; break;
+    case 2 : return 'S'; break;
   }
-  return computer_choice;
 }
 
 int FindWinner(char user_choice, char computer_choice)
@@ -32,47 +51,61 @@ int FindWinner(char user_choice, char computer_choice)
       case 'R' : return 0; break;
       case 'P' : return -1; break;
       case 'S' : return 1; break;
-    }
-    break;
+    } break;
 
     case 'P' : switch(computer_choice)
     {
       case 'R' : return 1; break;
       case 'P' : return 0; break;
       case 'S' : return -1; break;
-    }
-    break;
+    } break;
 
     case 'S' : switch(computer_choice)
     {
       case 'R' : return -1; break;
       case 'P' : return 1; break;
       case 'S' : return 0; break;
-    }
-    break;
+    } break;
   }
 }
 
 void main()
 {
-  char user_choice;
-  while(user_choice != 'Q')
+  // Included to seed radom number generation at runtime
+  srand((time(NULL)));
+
+  while(infinate)
   {
-    printf("Enter R for Rock, P for Paper, S for Scissors, Q for Quit\n");
 
-    user_choice = GetUserInput();
-    printf("\nyour choice: ", &user_choice);
-\
+    system("clear");
+    printf("╦═╗┌─┐┌─┐┬┌─  ╔═╗┌─┐┌─┐┌─┐┬─┐  ╔═╗┌─┐┬┌─┐┌─┐┌─┐┬─┐┌─┐\n");
+    printf("╠╦╝│ ││  ├┴┐  ╠═╝├─┤├─┘├┤ ├┬┘  ╚═╗│  │└─┐└─┐│ │├┬┘└─┐\n");
+    printf("╩╚═└─┘└─┘┴ ┴  ╩  ┴ ┴┴  └─┘┴└─  ╚═╝└─┘┴└─┘└─┘└─┘┴└─└─┘\n");
+    printf("=====================================================\n");
+    printf("Enter R for Rock, P for Paper, S for Scissors, Q to Quit\n\n");
+
+    char user_choice = GetUserInput();
+    if(user_choice == 'Q') { return; }
+    printf("your choice: %c \n", user_choice);
+
     char computer_choice = GetComputerChoice();
-    printf("\ncomputer choice: ", &computer_choice);
+    printf("computer choice: %c \n", computer_choice);
 
-    int winner = FindWinner(user_choice, computer_choice);
-
-    switch(winner)
+    switch(FindWinner(user_choice, computer_choice))
     {
-      case -1 : printf("\nThe Computer wins!"); break;
-      case 0 : printf("\nMatch tied!"); break;
-      case 1 : printf("\nYou win!"); break;
+      case -1 :
+      printf("The Computer wins!\n\n");
+      break;
+
+      case 0 :
+      printf("Match tied!\n\n");
+      break;
+
+      case 1 :
+      printf("You win!\n\n");
+      break;
     }
+
+    sleep(2);
   }
 }
